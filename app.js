472c7caddd4260a5216b6ba16ea4bcff5766e671
Alfex4936 <ikr@kakao.com>
2023-04-14 17:43:42 +0900

    Update

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 8089;
 
 app.get("/hello", (req, res) => {
   res.send("hello world");
 });
 
-app.listen(8089, () => {
-  console.log("listening on 8089");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
