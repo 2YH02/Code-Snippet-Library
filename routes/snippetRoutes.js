6fbe79a0b42f9ade07360056f0f478ac5365564f
Yonghun <yonghuni484@gmail.com>
2023-05-09 13:04:41 +0900

    update

--- routes/snippetRoutes.js
+++ routes/snippetRoutes.js
@@ -7,7 +7,7 @@ const snippetController = require("../controllers/snippetController");
 router.get("/", snippetController.getSnippets);
 router.get("/:id", snippetController.getSnippetsById);
 router.post("/new", snippetController.createSnippet);
-router.put("/update", snippetController.updateSnippet);
-router.delete("/delete", snippetController.deleteSnippet);
+router.put("/update/:id", snippetController.updateSnippet);
+router.delete("/delete/:id", snippetController.deleteSnippet);
 
 module.exports = router;
